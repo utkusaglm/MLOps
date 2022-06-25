6634ba7813c0284cdc51b4496b30a23dc745b217
utku saglam <saglmutku@gmail.com>
2022-06-25 11:31:33 +0300

    gcp

--- object-detection/obd.py
+++ object-detection/obd.py
@@ -1,7 +1,6 @@
 import io
 import uvicorn
 import numpy as np
-import nest_asyncio
 from enum import Enum
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.responses import StreamingResponse
